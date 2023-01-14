8c437e87594e65c14c0dc0d00105b4c810e72098
Andrey Sereda <andreysereda@ya.ru>
2023-01-15 01:23:24 +0300

    Step 15. Add CLI arguments

--- server.py
+++ server.py
@@ -1,19 +1,19 @@
-from os import path, getpgid
+from os import path, getpgid, getenv
+from sys import exit
 from aiohttp import web
 import aiofiles
 import asyncio
 import datetime
 import logging
+import argparse
 
 
 INTERVAL_SECS = 1
 DIR = '/home/andrey/5_My_projects/Dvmn/photozip/'
 TEST_DIR = f'{DIR}Dvmn/'
 SERVER_DIR = f'{DIR}async-download-service/test_photos/'
-
-logging.basicConfig(
-    format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
-    level=logging.DEBUG)
+LOG_LEVEL = logging.WARNING
+LOG_FORMAT = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s'
 
 
 async def uptime_handler(request):
@@ -40,15 +40,17 @@ async def uptime_handler(request):
 async def archivate(request):
     archive_hash = request.match_info.get('archive_hash', "7kna")
     logging.debug(f'ARCH_HASH: {archive_hash}')
-    path_exists = path.exists(f'{SERVER_DIR}{archive_hash}')
-    logging.debug(f'Path exists: {path_exists}')
+
+    path_exists = path.exists(f'{archive_path}{archive_hash}')
     if not path_exists:
         logging.error("Path doesn't exist")
         raise web.HTTPNotFound(
-            text="Sorry. Archive you are asking for doesn't exist or was deleted")
+            text="Sorry. Archive you are asking for doesn't exist")
+    logging.debug(f'Path exists: {path_exists}')
     response = web.StreamResponse()
     response.headers['Content-Type'] = 'application/octet-stream'
-    response.headers['Content-Disposition'] = f'attachment; filename="{archive_hash}.zip"'
+    content_disposition = f'attachment; filename="{archive_hash}.zip"'
+    response.headers['Content-Disposition'] = content_disposition
 
     # Отправляет клиенту HTTP заголовки
     await response.prepare(request)
@@ -75,7 +77,8 @@ async def archivate(request):
             # Отправляет клиенту очередную порцию ответа
             await response.write(stdout)
             # Пауза для проверки разрыва соединения по инициативе клиента
-            await asyncio.sleep(1)
+            if is_throttling_on:
+                await asyncio.sleep(INTERVAL_SECS)
 
     except (asyncio.CancelledError, SystemExit):
         logging.debug('Download was interrupted')
@@ -101,11 +104,59 @@ async def handle_index_page(request):
 
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+            description='Web-server for downloading photoarchives')
+    parser.add_argument(
+            'path',
+            nargs='?',
+            help='path to the directory with photoarchives'
+            )
+    parser.add_argument(
+            '-l',
+            '--logging',
+            action='store_true',
+            help='turn on detailed logging'
+            )
+    parser.add_argument(
+            '-t',
+            '--throttling',
+            action='store_true',
+            help='turn on pause between packages to emulate slow connection'
+            )
+    args = parser.parse_args()
+    print(args)
+
+    global is_throttling_on
+    is_throttling_on = args.throttling
+
+    global archive_path
+    if args.path:
+        if args.path.startswith('/'):
+            archive_path = args.path
+        else:
+            archive_path = f'{getenv("PWD")}/{args.path}'
+        if not archive_path.endswith('/'):
+            archive_path += '/'
+    else:
+        archive_path = SERVER_DIR
+
+    if not path.exists(archive_path):
+        logging.error("Provided path doesn't exist")
+        exit(1)
+
+    if args.logging:
+        LOG_LEVEL = logging.DEBUG
+    logging.basicConfig(
+        format=LOG_FORMAT,
+        level=LOG_LEVEL)
+
+    logging.debug("This is debug log message")
+
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
         web.get('/archive/{archive_hash}/', archivate),
         web.get('/uptime/', uptime_handler),
     ])
-    # try:
     web.run_app(app)
